fac5bf616386e572c63cd23cf0f21070b38ce11e
amrancz <adam@amran.cz>
2020-08-24 19:18:01 +0200

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,20 @@ module.exports = {
       title: `Adam Amran's notes`,
     },
     plugins: [
-        {
-          resolve: `gatsby-theme-andy`,
-          options: {
-            hideDoubleBrackets: true,
-          },
+      {
+        resolve: `gatsby-theme-andy`,
+        options: {
+          hideDoubleBrackets: true,
+          mdxOtherwiseConfigured: true,
         },
-      ],
+      },
+      `gatsby-plugin-postcss`,
+      {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          extensions: ['.mdx', '.md'],
+          gatsbyRemarkPlugins: [`gatsby-remark-embedder`],
+        },
+      },
+    ],
   };
